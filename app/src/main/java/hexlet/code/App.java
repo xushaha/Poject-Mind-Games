5114bdce3b999af24aa36ee179c6566d5ed8af32
xushaha <xushaha@ya.ru>
2022-10-31 18:34:04 +0300

    fifth step_part one

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,23 @@
 package hexlet.code;
-
+import java.util.Scanner;
 
 
 public class App {
     public static void main(String[] args) {
-        Cli.greeting();
 
+        Scanner gameChoice = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+
+        int choice = gameChoice.nextInt();
+
+        System.out.println("Your choice: " + choice);
+
+        if (choice == 1) {
+            Cli.greeting();
+        }
+        gameChoice.close();
     }
 }
